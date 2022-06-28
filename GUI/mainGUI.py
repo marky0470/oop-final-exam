9049c7edd8154e46762a281e21320f07703d5500
Francis James Tolentino <francistolentino1107@gmail.com>
2022-06-28 23:29:16 +0800

    mysql connection

--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -3,4 +3,3 @@ class MainGUI():
 
     def __init__(self):
         print('mainGUI')
-        pass
---
+++ connection.py
@@ -0,0 +1,11 @@
+
+import mysql.connector as mysql
+
+
+connection = mysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    port="3306",
+    database="test"
+)
